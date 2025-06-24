60db77d152f9852846c9cb526486e0c36a46ab3f
plr7scripts <ricardofrnc@icloud.com>
2025-06-24 02:03:47 -0300

    Update cavarumburaco.lua

--- cavarumburaco.lua
+++ cavarumburaco.lua
@@ -6,5 +6,10 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DigEvent"):FireServer(unpack(args))
+local args = {
+    [1] = 3
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(args))
 wait(0.00001)
 end
